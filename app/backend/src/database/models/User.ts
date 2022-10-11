fecffdda4880e17177eb875b6813941467e83abd
Gabriel Alves <gabrielalves@outlook.com>
2022-10-11 01:39:20 -0300

    feat: create the sequelize user model

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,42 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  role: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  password: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'user',
+  timestamps: false,
+});
+
+export default User;
