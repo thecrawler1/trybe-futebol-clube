9750935bd416debcb2cb3e2a8fb6635ffa349e47
Gabriel Alves <gabrielalves@outlook.com>
2022-10-11 20:41:36 -0300

    feat: create the sequelize team model

---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,27 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  team_name: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'team',
+  timestamps: false,
+});
+
+export default Team;
