889efaa0b8798dbef712d0c303ffc64fadd5dbfa
Gabriel Alves <gabrielalves@outlook.com>
2022-10-12 23:09:06 -0300

    feat: add the /teams/:id get endpoint

--- app/backend/src/routers/teamRouter.ts
+++ app/backend/src/routers/teamRouter.ts
@@ -2,10 +2,13 @@ import { Router } from 'express';
 
 import RouteAdapter from '../adapters/express/RouteAdapter';
 import GetAllTeamsControllerFactory from '../factories/GetAllTeamsControllerFactory';
+import GetTeamByIdControllerFactory from '../factories/GetTeamByIdControllerFactory';
 
 const router = Router();
 const getAllTeamsController = GetAllTeamsControllerFactory.make();
+const getTeamByIdController = GetTeamByIdControllerFactory.make();
 
 router.get('/', RouteAdapter.adapt(getAllTeamsController));
+router.get('/:id', RouteAdapter.adapt(getTeamByIdController));
 
 export default router;
