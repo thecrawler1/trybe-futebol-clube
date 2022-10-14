dc1bbf1cc544b913baa99faff9ca0c0ad45de99e
Gabriel Alves <gabrielalves@outlook.com>
2022-10-14 15:57:14 -0300

    feat: add the post /matches endpoint

--- app/backend/src/routers/matchRouter.ts
+++ app/backend/src/routers/matchRouter.ts
@@ -2,10 +2,14 @@ import { Router } from 'express';
 
 import RouteAdapter from '../adapters/express/RouteAdapter';
 import GetAllMatchesControllerFactory from '../factories/GetAllMatchesControllerFactory';
+import CreateMatchControllerFactory from '../factories/CreateMatchControllerFactory';
+import auth from '../middlewares/auth';
 
 const router = Router();
 const getAllMatchesController = GetAllMatchesControllerFactory.make();
+const createMatchController = CreateMatchControllerFactory.make();
 
 router.get('/', RouteAdapter.adapt(getAllMatchesController));
+router.post('/', auth, RouteAdapter.adapt(createMatchController));
 
 export default router;
