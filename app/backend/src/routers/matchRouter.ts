e5e2b6e7caaec3f2584c61ea8a7ab1db94dd2b9b
Gabriel Alves <gabrielalves@outlook.com>
2022-10-14 16:03:18 -0300

    feat: add the patch /matches/:id/finish endpoint

--- app/backend/src/routers/matchRouter.ts
+++ app/backend/src/routers/matchRouter.ts
@@ -3,13 +3,16 @@ import { Router } from 'express';
 import RouteAdapter from '../adapters/express/RouteAdapter';
 import GetAllMatchesControllerFactory from '../factories/GetAllMatchesControllerFactory';
 import CreateMatchControllerFactory from '../factories/CreateMatchControllerFactory';
+import FinishMatchControllerFactory from '../factories/FinishMatchControllerFactory';
 import auth from '../middlewares/auth';
 
 const router = Router();
 const getAllMatchesController = GetAllMatchesControllerFactory.make();
 const createMatchController = CreateMatchControllerFactory.make();
+const finishMatchController = FinishMatchControllerFactory.make();
 
 router.get('/', RouteAdapter.adapt(getAllMatchesController));
 router.post('/', auth, RouteAdapter.adapt(createMatchController));
+router.patch('/:id/finish', auth, RouteAdapter.adapt(finishMatchController));
 
 export default router;
